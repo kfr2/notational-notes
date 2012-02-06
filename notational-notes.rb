16720fc36b2ac121fb7ffa1fca18f73c58e909e2
Kevin Richardson <kevin@magically.us>
2012-02-05 21:17:18 -0500

    added a very small set of further options

--- notational-notes.rb
+++ notational-notes.rb
@@ -3,22 +3,32 @@
 # https://github.com/kfredrichardson/notational-notes
 #
 # Converts a directory of notes in text format (like those produced by Notational Velocity/nvAlt)
-# into a notes webpage in the style of (url)
+# into a notes webpage in the style of (url).  Currently supports Markdown formatting syntax.
+# Other formatting syntaxes (Textile, etc) should not be particularly difficult to implement.
 #
 # Optional:  if hide_private is set to true, the script will ignore all files beginning with "private"
+
 require 'rdiscount'
 
 # The location of your NV files.
 note_directory   = "/Users/kevin/Dropbox/notational/"
 # Where you would like the output files placed.
-output_directory = "/Users/kevin/notes/"
+output_directory = "/Users/kevin/Developer/web/lotusgarden/notes/"
+# -- [ Optional ] --
 # Ignore files beginning with "private".
-hide_private     = true
+hide_private = true
+# The name to display in the output's header.
+user_name = "Kevin Richardson"
+# The URI of your website.
+web_uri = "http://kevin.magically.us"
 
+hide_private = false if hide_private.nil?
+user_name = "Notational Note User" if user_name.nil?
+web_uri = "https://github.com/kfredrichardson/notational-notes" if web_uri.nil?
 
 Dir.chdir(note_directory)
 notes_index = File.open(output_directory + "index.htm", "w")
-notes_index.puts "<html><head><title>notational notes!</title></head><body><h1><a href='https://github.com/kfredrichardson/notational-notes' title='Notational Notes'>Notational Notes</a>!  Enjoy.</h1><hr><ul>"
+notes_index.puts "<html><head><title>notational notes!</title></head><body><h1><a href='#{web_uri}' title='homepage'>#{user_name}\'s</a> Public Notes!  Enjoy.</h1><hr><ul>"
 
 Dir["*.txt"].each do |file|
     # Ignore files marked as private (if set by user).
@@ -34,7 +44,7 @@ Dir["*.txt"].each do |file|
     # Truncate the ".txt" off the filename.
     file = file.slice(0..-5)
 
-    output = "<html><head><title>#{file}</title></head><body><a href='index.htm'>Notes Home</a>" + markdown.to_html + "</body></html>"
+    output = "<html><head><title>#{file}</title></head><body><a href='index.htm'>Notes Home</a><hr>" + markdown.to_html + "</body></html>"
 
     # Write the note to the output_directory.
     File.new(output_directory + file + ".htm", "w").puts output
@@ -42,7 +52,7 @@ Dir["*.txt"].each do |file|
     notes_index.puts "<li><a href='#{file}.htm' title='#{file}'>#{file}</a></li>"
 end
 
-notes_index.puts "</ul></body></html>"
+notes_index.puts "</ul><hr>Powered by <a href='https://github.com/kfredrichardson/notational-notes' title='Notational Notes'>Notational Notes</a></body></html>"
 notes_index.close
 
 puts "Done converting notes to HTML."
