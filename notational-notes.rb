7b5d0bbb6deecb8075e8855c030a0f3c1d2922d5
Kevin Richardson <kevin@magically.us>
2012-02-14 21:42:38 -0500

    Added display of note modification dates.

--- notational-notes.rb
+++ notational-notes.rb
@@ -30,6 +30,10 @@ Dir.chdir(note_directory)
 notes_index = File.open(output_directory + "index.htm", "w")
 notes_index.puts "<html><head><title>notational notes!</title></head><body><h1><a href='#{web_uri}' title='homepage'>#{user_name}\'s</a> Public Notes!  Enjoy.</h1><hr><ul>"
 
+# Holds the latest update time for display in the index file.
+# Defaults to a very old year (0).
+newest_time = Time.new(0)
+
 Dir["*.txt"].each do |file|
     # Ignore files marked as private (if set by user).
     if file =~ /\Aprivate/i
@@ -38,13 +42,15 @@ Dir["*.txt"].each do |file|
 
     # Convert the note's content to Markdown.
     content  = File.open(file, "r")
+    modified = content.mtime 
+    newest_time = modified if modified > newest_time
     markdown = RDiscount.new(content.read)
     content.close
 
     # Truncate the ".txt" off the filename.
     file = file.slice(0..-5)
 
-    output = "<html><head><title>#{file}</title></head><body><a href='index.htm'>Notes Home</a><hr>" + markdown.to_html + "</body></html>"
+    output = "<html><head><title>#{file}</title></head><body><a href='index.htm'>Notes Home</a><hr>" + markdown.to_html + "<footer><hr><em>Last modified: #{modified.to_s}</em></footer></body></html>"
 
     # Write the note to the output_directory.
     File.new(output_directory + file + ".htm", "w").puts output
@@ -52,7 +58,7 @@ Dir["*.txt"].each do |file|
     notes_index.puts "<li><a href='#{file}.htm' title='#{file}'>#{file}</a></li>"
 end
 
-notes_index.puts "</ul><hr>Powered by <a href='https://github.com/kfredrichardson/notational-notes' title='Notational Notes'>Notational Notes</a></body></html>"
+notes_index.puts "</ul><footer><hr><em>Last modified: #{newest_time.to_s}</em><p>Powered by <a href='https://github.com/kfredrichardson/notational-notes' title='Notational Notes'>Notational Notes</a></p></footer></body></html>"
 notes_index.close
 
 puts "Done converting notes to HTML."
